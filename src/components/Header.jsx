3161b32194f6075d89a3c8597d8e51e6851b5072
Myat Inzally Khine <myatinzallykhine@gmail.com>
2024-07-30 11:57:41 -0700

    layout changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,17 @@ import Menu from '../assets/images/shared/icon-hamburger.svg';
 import { Link } from 'react-router-dom';
 
 
+
 function Header(){
     return(
 
-        <div className="flex ml-10 inline justify-between pt-10">
+        <div className="flex ml-10 inline justify-between pt-10 ">
             <img src={Logo} alt='Logo' className='mr-5 w-8 h-8'/>
             
             <div className='md:48 border-t border-gray-300 mt-6 hidden sm:visible'></div>
 
            
-            <div className="p-3  md:backdrop-blur-sm md:bg-white/30 hidden sm:visible">
+            <div className="p-3  md:backdrop-blur-sm md:bg-white/30 invisible sm:visible">
                 <ul className='flex inline mr-10 space-x-8 font-secondary text-base text-slate-200'>
                     <Link to="/"><li className='hover:underline hover:underline-offset-8'>00 HOME</li></Link>
                     <Link to="/destination"><li className='hover:underline hover:underline-offset-8'>01 DESTINATION</li></Link>
@@ -24,7 +25,7 @@ function Header(){
             </div>
             
             
-            <img src={Menu} alt='hamburger menu' className='visible md:hidden w-8 h-8 mr-12'/>
+            <img src={Menu} alt='hamburger menu' className='visible md:hidden w-8 h-8 absolute right-10'/>
             
 
 
