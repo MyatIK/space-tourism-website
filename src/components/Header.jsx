1bd1b11990fdeaf2988ffd7a5faf0cc246f25ee3
Myat Inzally Khine <myatinzallykhine@gmail.com>
2024-07-30 18:28:11 -0700

    sidebar toggle added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,20 @@
 import Logo from '../assets/images/shared/logo.svg';
 import Menu from '../assets/images/shared/icon-hamburger.svg';
+import Closed from '../assets/images/shared/icon-close.svg';
 import { Link } from 'react-router-dom';
-
+import { useState } from 'react';
 
 
 function Header(){
+
+    const[isOpen,setIsOpen]=useState(false);
+
+    const ToggleSide =() =>{
+        isOpen === true ? setIsOpen(false): setIsOpen(true);
+    }
+
     return(
+        
 
         <div className="flex ml-10 inline justify-between pt-10 relative">
             <img src={Logo} alt='Logo' className='mr-5 w-8 h-8'/>
@@ -23,9 +32,32 @@ function Header(){
                 </ul>
             
             </div>
+            {isOpen ? 
+            <button>
+                <img src={Menu} alt='hamburger menu' className='visible md:hidden w-8 h-8 absolute right-10' onClick={ToggleSide}/>
             
+            </button>:
+
+            <div className="w-3/4 h-screen backdrop-blur-sm bg-white/10 visible md:invisible absolute right-0 top-0">
+                <button className='absolute right-5 top-5'>
+                    <img src={Closed} alt="exit button" onClick={ToggleSide}/>
+
+                </button>
+
+                <ul className='mr-10 space-y-10 font-secondary h-screen text-base text-slate-200 text-left'>
+                    <Link to="/"><li className='hover:underline hover:underline-offset-8'>00 HOME</li></Link>
+                    <Link to="/destination"><li className='hover:underline hover:underline-offset-8'>01 DESTINATION</li></Link>
+                    <Link to="/crew"><li className='hover:underline hover:underline-offset-8'>02 CREW</li></Link>
+                    <Link to="/technology"><li className='hover:underline hover:underline-offset-8'>03 TECHNOLOGY</li></Link>
+
+                </ul>
+
+            </div>
             
-            <img src={Menu} alt='hamburger menu' className='visible md:hidden w-8 h-8 absolute right-10'/>
+        }
+            
+        
+            
             
 
 
